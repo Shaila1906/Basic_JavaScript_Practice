0a5cc5db6b7c0a698caae3acc1bd7ac715b5a2d9
Shaila Akter <shailarima1999@gmail.com>
2025-09-09 07:13:11 +0600

    second commit

---
+++ first.js
@@ -0,0 +1,11 @@
+function createGreeting(name) {
+  const greeting = `Hello, ${name}!`;
+  return greeting;
+}
+
+function name(params) {}
+
+function sayHello(name) {
+  const greeting = `Hello, ${name}!`;
+  return greeting;
+}
