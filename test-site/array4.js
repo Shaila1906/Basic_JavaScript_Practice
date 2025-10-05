b0647f8b7f1fab24dd8cafc5f0b551122c381ddc
Shaila Akter <53009553+Shaila1906@users.noreply.github.com>
2025-10-05 10:37:32 +0600

    Remove element and filter elements based on .startsWith()

--- test-site/array4.js
+++ test-site/array4.js
@@ -1,3 +1,7 @@
+/*
+    Find the index of the "Eagles" item, and use that to remove the "Eagles" item.
+    Make a new array from this one, called eBirds, that contains only birds from the original array whose names begin with the letter "E". Note that startsWith() is a great way to check whether a string starts with a given character.
+*/
 const birds = ["Parrots", "Falcons", "Eagles", "Emus", "Caracaras", "Egrets"];
 
 let eagleIndex = birds.indexOf("Eagles");
