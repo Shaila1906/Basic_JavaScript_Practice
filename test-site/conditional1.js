30fe972e3c68d3fc630dd35006551d572a03bc41
Shaila Akter <53009553+Shaila1906@users.noreply.github.com>
2025-10-06 14:17:24 +0600

    Basic if else

---
+++ test-site/conditional1.js
@@ -0,0 +1,8 @@
+let season = "summer";
+let response;
+
+if( season==="summer"){response= "It's warm!";}
+else if(season==="winter"){ response = "It's cold!"}
+else{ response = "What season is this?"};
+
+console.log(response);
